91003a2380cefbb0cfbc7447aedcda60ceb5a673
jxh0531 <jarred.harkness@gmail.com>
2021-09-21 11:45:00 +0200

    Add navbar

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import './Styles/App.css';
-import Palette from './Components/Palette'
-import seedColors from './seedColors'
-import { generatePalette } from './colorHelpers';
+import "./Styles/App.css";
+import Palette from "./Components/Palette";
+import seedColors from "./seedColors";
+import { generatePalette } from "./colorHelpers";
 
 function App() {
   return (
     <div>
-      <Palette palette={generatePalette(seedColors[3])}/>
+      <Palette palette={generatePalette(seedColors[3])} />
     </div>
-  )
+  );
 }
 
 export default App;
--- src/Components/Palette.js
+++ src/Components/Palette.js
@@ -1,36 +1,35 @@
-import React, { Component } from 'react';
-import ColorBox from './ColorBox'
-import '../Styles/Palette.css'
-import Slider from 'rc-slider'
-import 'rc-slider/assets/index.css'
+import React, { Component } from "react";
+import ColorBox from "./ColorBox";
+import NavBar from "./NavBar";
+import "../Styles/Palette.css";
 
-export default class Palette extends Component{
-    constructor(props){
-        super(props);
-        this.state = {level:500};
-        this.changeLevel = this.changeLevel.bind(this)
-    }
+export default class Palette extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { level: 500 };
+    this.changeLevel = this.changeLevel.bind(this);
+  }
 
-    changeLevel(newLevel) {
-        this.setState({level:newLevel});
-    }
+  changeLevel(newLevel) {
+    this.setState({ level: newLevel });
+  }
 
-    render() {
-        const {colors} = this.props.palette
-        const {level} = this.state
-        const colorBoxes = colors[level].map(color => (
-            <ColorBox background = {color.hex} name={color.name}/>
-        ))
-        return (
-            <div className="Palette">
-                {/*Navbar goes here */}
-                <Slider defaultValue={level} min = {100} max= {900} step={100} onAfterChange={this.changeLevel} />
-                <div className="Palette-colors">
-                {/*Color box goes here */}
-                {colorBoxes}
-                </div>
-                {/*footer*/}
-            </div>
-        )
-    }
+  render() {
+    const { colors } = this.props.palette;
+    const { level } = this.state;
+    const colorBoxes = colors[level].map((color) => (
+      <ColorBox background={color.hex} name={color.name} />
+    ));
+    return (
+      <div className="Palette">
+        {/*Navbar goes here */}
+        <NavBar level={level} changeLevel={this.changeLevel} />
+        <div className="Palette-colors">
+          {/*Color box goes here */}
+          {colorBoxes}
+        </div>
+        {/*footer*/}
+      </div>
+    );
+  }
 }
