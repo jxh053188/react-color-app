da9524b965e41c994a7c67b3ad1b7139ddead9ab
jxh0531 <jarred.harkness@gmail.com>
2021-09-21 09:40:17 +0200

    Initial commit

---
+++ src/Palette.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class Palette extends Component{
+    render() {
+        return (
+            <div className="Palette">
+                {/*Navbar goes here */}
+                <div className="Palette-colors">
+                {/*Color box goes here */}
+                </div>
+                {/*footer*/}
+            </div>
+        )
+    }
+}
