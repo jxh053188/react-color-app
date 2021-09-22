c76be8f6b0430dcf9fbbb4d8f1189040045cade1
jxh0531 <jarred.harkness@gmail.com>
2021-09-22 10:17:46 +0200

    Added links to minipalettes

--- src/Components/MiniPalette.js
+++ src/Components/MiniPalette.js
@@ -9,12 +9,16 @@ const styles = {
     padding: "0.5rem",
     position: "relative",
     overflow: "hidden",
-    "&:hover:": {
+    "&:hover": {
       cursor: "pointer",
     },
   },
   colors: {
-    backgroundColor: "grey",
+    backgroundColor: "white",
+    height: "180px",
+    width: "100%",
+    borderRadius: "5px",
+    overflow: "hidden",
   },
   title: {
     display: "flex",
@@ -30,17 +34,31 @@ const styles = {
     marginLeft: "0.5rem",
     fontSize: "1.5rem",
   },
+  miniColor: {
+    height: "25%",
+    width: "20%",
+    display: "inline-block",
+    margin: "0 auto",
+    position: "relative",
+    marginBottom: "-4.5px",
+  },
 };
 
 function MiniPalette(props) {
-  const { classes, paletteName, emoji } = props;
+  const { classes, paletteName, emoji, colors } = props;
+  const miniColorBoxes = colors.map((color) => (
+    <div
+      className={classes.miniColor}
+      style={{ backgroundColor: color.color }}
+      key={color.name}
+    ></div>
+  ));
   return (
-    <div className={classes.root}>
-      <div className={classes.colors}>
-        <h5 className={classes.title}>
-          {paletteName} <span className={classes.emoji}>{emoji}</span>
-        </h5>
-      </div>
+    <div className={classes.root} onClick={props.handleClick}>
+      <div className={classes.colors}> {miniColorBoxes}</div>
+      <h5 className={classes.title}>
+        {paletteName} <span className={classes.emoji}>{emoji}</span>
+      </h5>
     </div>
   );
 }
